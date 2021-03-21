329cc620969019c38ac7bd16862e94742d4cf49c
Aaron <AaronBatilo@gmail.com>
2021-03-21 11:05:28 -0600

    Write tests that verify message model assumptions
    The first assumption is that we'll always return the messages with the
    newest messages being first.
    
    The second test is to verify logical behavior in whether or not a
    message is readable/available for the client side.

--- backend/messages/tests.py
+++ backend/messages/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from datetime import datetime, timedelta
+from rest_framework.test import APITestCase, APIClient
+
+from django.utils.timezone import make_aware
+import pytz
+
+from .models import Message
 
 # Create your tests here.
+class MessageTest(APITestCase):
+    def setUp(self) -> None:
+        now = datetime.utcnow()
+        one_week_earlier = make_aware(
+            datetime.utcnow() + timedelta(weeks=-1), timezone=pytz.UTC
+        )
+        one_week_later = make_aware(
+            datetime.utcnow() + timedelta(weeks=1), timezone=pytz.UTC
+        )
+        Message.objects.create(
+            title="Message 1", content="Message 1", revealed_date=one_week_earlier
+        )
+        Message.objects.create(
+            title="Message 2", content="Message 2", revealed_date=one_week_later
+        )
+
+    def test_messages_are_in_desc_created_date_order(self):
+        response = self.client.get("/api/v1/messages")
+        dates = [message["created_date"] for message in response.data["results"]]
+        self.assertListEqual(dates, sorted(dates, reverse=True))
+
+    def test_server_side_revealed(self):
+        response = self.client.get("/api/v1/messages")
+        messages = response.data["results"]
+        message_1 = messages[1]
+        message_2 = messages[0]
+
+        self.assertEquals(message_1["title"], "Message 1")
+        self.assertTrue(message_1["revealed"])
+        self.assertEquals(message_1["content"], "Message 1")
+
+        self.assertEquals(message_2["title"], "Message 2")
+        self.assertFalse(message_2["revealed"])
+        self.assertEquals(message_2["content"], "Unavailable")
