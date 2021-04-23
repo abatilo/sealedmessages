2301334c59167d4858ab3b1a03d572f652fe2e02
Aaron <AaronBatilo@gmail.com>
2021-04-23 07:20:01 -0600

    Simplify ApplicationShell styling and div layout

--- frontend/src/Components/ApplicationShell.tsx
+++ frontend/src/Components/ApplicationShell.tsx
@@ -8,28 +8,20 @@ type Props = {
 
 const ApplicationShell = ({ children, pageHeader }: Props) => {
   return (
-    <div>
-      <div className="min-h-screen bg-gray-50">
-        <div className="py-4">
-          <header className="pb-2">
-            <div className="px-4 max-w-7xl sm:px-6 lg:px-8 mb-2">
-              <div className="flex">
-                <LockClosedIcon className="w-8 h-8 mr-1" />
-                <div>
-                  <h1 className="text-3xl font-bold leading-tight text-gray-900">
-                    {pageHeader}
-                  </h1>
-                  <h1 className="text-xl leading-tight text-gray-500">
-                    Save a message for later
-                  </h1>
-                </div>
-              </div>
-            </div>
-            <hr className="mx-8" />
-          </header>
-          <main className="py-2 mx-20 sm:px-6 lg:px-8">{children}</main>
+    <div className="bg-gray-50 px-4 py-6">
+      <div className="flex mb-2">
+        <LockClosedIcon className="w-8 h-8 mr-1" />
+        <div>
+          <h1 className="text-3xl font-bold leading-tight text-gray-900">
+            {pageHeader}
+          </h1>
+          <h1 className="text-xl leading-tight text-gray-500">
+            Save a message for later
+          </h1>
         </div>
       </div>
+      <hr className="mx-8" />
+      <main className="py-2 mx-20 sm:px-6 lg:px-8">{children}</main>
     </div>
   );
 };
