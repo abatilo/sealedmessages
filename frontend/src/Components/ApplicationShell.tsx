2e8be4abe397878713908d437d89d67f0f1245ba
Aaron <AaronBatilo@gmail.com>
2021-04-23 07:23:55 -0600

    Make header link to home page

--- frontend/src/Components/ApplicationShell.tsx
+++ frontend/src/Components/ApplicationShell.tsx
@@ -1,5 +1,6 @@
 import { LockClosedIcon } from "@heroicons/react/outline";
 import React, { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
   children?: ReactNode;
@@ -8,16 +9,20 @@ type Props = {
 
 const ApplicationShell = ({ children, pageHeader }: Props) => {
   return (
-    <div className="bg-gray-50 px-4 py-6">
+    <div className="min-h-screen bg-gray-50 px-4 py-6">
       <div className="flex mb-2">
-        <LockClosedIcon className="w-8 h-8 mr-1" />
+        <Link to="/">
+          <LockClosedIcon className="w-8 h-8 mr-1" />
+        </Link>
         <div>
-          <h1 className="text-3xl font-bold leading-tight text-gray-900">
-            {pageHeader}
-          </h1>
-          <h1 className="text-xl leading-tight text-gray-500">
-            Save a message for later
-          </h1>
+          <Link to="/">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              {pageHeader}
+            </h1>
+            <h1 className="text-xl leading-tight text-gray-500">
+              Save a message for later
+            </h1>
+          </Link>
         </div>
       </div>
       <hr className="mx-8" />
